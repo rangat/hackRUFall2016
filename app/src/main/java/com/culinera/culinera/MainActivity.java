e6a7a57a28fc38803facbf51020c08e05be69981
ArjunBansil <bansilarjun@gmail.com>
2016-10-22 19:12:22 -0400

    Restart

--- app/src/main/java/com/culinera/culinera/MainActivity.java
+++ app/src/main/java/com/culinera/culinera/MainActivity.java
@@ -27,108 +27,15 @@ import java.util.ArrayList;
 import tools.DeviceScanListAdapter;
 
 public class MainActivity extends AppCompatActivity {
-    SwipeRefreshLayout swp;
-    private static final int REQ_BLUETOOTH_ENABLE = 1000;
-    private static final int DEVICE_SCAN_MILLISECONDS = 100private DeviceScanListAdapter mDeviceAdapter;
-    private BroadcastReceiver mReceiver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Dexter.initialize(getApplicationContext());
-        MultiplePermissionsListener dl =
-                DialogOnAnyDeniedMultiplePermissionsListener.Builder
-                    .withContext(getApplicationContext())
-                    .withTitle("Bluetooth & file storage permission")
-                    .withMessage("Both bluetooth and file storage are necessary for the application to work")
-                    .withButtonText("Re-do")
-                    .withIcon(R.mipmap.ic_launcher)
-                    .build();
-         Dexter.checkPermissions(dl, Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.READ_EXTERNAL_STORAGE,
-                 Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
-        swp = (SwipeRefreshLayout)findViewById(R.id.swiperefresh);
-        mReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                String action = intent.getAction();
-                if(BluetoothDevice.ACTION_FOUND.equals(action)){
-                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                    Log.i("Cancer", "Device: " + device.getName());
-                }
-                Log.i("Cancer", "Nothing was scanned");
-            }
-        };
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        Log.i("TAG", "About to scan");
-        registerReceiver(mReceiver, filter);
-        /*
-
-        checkBluetoothSupport();
-
-        swp.setOnRefreshListener(this::startScanning);
-        mDeviceAdapter = new DeviceScanListAdapter(this);
-        ListView l = (ListView)findViewById(R.id.bluetoothList);
-        l.setAdapter(mDeviceAdapter);
-        */
     }
 
-    @Override
-    protected void onResume(){
-        super.onResume();
-//        mDeviceAdapter.clear();
-        swp.setRefreshing(true);
-        startScanning();
-    }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-        stopScanning();
-    }
-
-    private void startScanning(){
-        Log.i("Blueteeth", "Start scanning");
-      //  mDeviceAdapter.clear();
-        BlueteethManager.with(this).scanForPeripherals(DEVICE_SCAN_MILLISECONDS, bledevices -> {
-            Log.i("Blueteeth", "Scan completed");
-            swp.setRefreshing(false);
-            for(BlueteethDevice device :bledevices){
-                if(!TextUtils.isEmpty(device.getBluetoothDevice().getName())){
-                    Log.i("Blueteeth", device.getName() + " "+ device.getMacAddress());
-                    mDeviceAdapter.add(device);
-                }
-            }
-        });
-    }
-
-    private void stopScanning(){
-        swp.setRefreshing(false);
-        BlueteethManager.with(this).stopScanForPeripherals();
-    }
-
-    private void checkBluetoothSupport(){
-        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)){
-            Log.e("Error","No bluetooth support");
-        }
-        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
-        if(btAdapter == null){
-            Log.e("Error", "No BLE support");
-        }
-        if(!btAdapter.isEnabled()){
-            enableBluetooth();
-        }
-    }
-
-    @Override
-    protected  void onDestroy(){
-        unregisterReceiver(mReceiver);
-    }
-
-    private void enableBluetooth(){
-        startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), REQ_BLUETOOTH_ENABLE);
-    }
 
 
 }
