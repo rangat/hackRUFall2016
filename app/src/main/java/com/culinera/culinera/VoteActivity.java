fd8ca38857fc0e70dc745497a4d09223f59b0f32
ArjunBansil <bansilarjun@gmail.com>
2016-10-22 22:51:31 -0400

    Prepping for JSON data

---
+++ app/src/main/java/com/culinera/culinera/VoteActivity.java
@@ -0,0 +1,14 @@
+package com.culinera.culinera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vote);
+
+    }
+}
