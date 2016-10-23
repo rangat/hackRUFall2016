58116677c3932f800b7e0b78f588fceccb6996af
Kevin Gartner <kevin1997@me.com>
2016-10-22 22:23:58 -0400

    Firebase integration #2

--- app/src/main/java/tools/JSONtoArrayList.java
+++ app/src/main/java/tools/JSONtoArrayList.java
@@ -1,8 +1,16 @@
 package tools;
 
+import java.util.ArrayList;
+import java.lang.Object;
 /**
  * Created by Arjun Bansil on 10/22/2016.
+ * Firebase URL: https://hackrufall2016.firebaseio.com/
  */
 
 public class JSONtoArrayList {
+    public static void main(String []args){
+
+    }
 }
+
+
