3a6779527fab7a78c1eb37057a8b2899ada9c046
ArjunBansil <bansilarjun@gmail.com>
2016-10-22 21:57:56 -0400

    Made the front page UI

--- app/src/main/java/com/culinera/culinera/MainActivity.java
+++ app/src/main/java/com/culinera/culinera/MainActivity.java
@@ -8,11 +8,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import com.karumi.dexter.Dexter;
@@ -29,6 +32,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CardView vote = (CardView)findViewById(R.id.signedPic_card);
+        CardView view = (CardView)findViewById(R.id.viewData_card);
+        vote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Start voting bro", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "View voting data", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+
 
     }
 
