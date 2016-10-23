8b43f7211a7a07b35f7545fe3b933650ded14e05
ArjunBansil <bansilarjun@gmail.com>
2016-10-23 01:58:29 -0400

    Working on JSON read

--- app/src/main/java/com/culinera/culinera/MainActivity.java
+++ app/src/main/java/com/culinera/culinera/MainActivity.java
@@ -1,6 +1,7 @@
 package com.culinera.culinera;
 
 import android.Manifest;
+import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,10 +25,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         CardView vote = (CardView)findViewById(R.id.signedPic_card);
         CardView view = (CardView)findViewById(R.id.viewData_card);
+        Intent intent = new Intent(this, VoteActivity.class);
         vote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Start voting bro", Snackbar.LENGTH_SHORT).show();
+                startActivity(intent);
             }
         });
         view.setOnClickListener(new View.OnClickListener() {
