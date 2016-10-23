81ffbdcb773bdb82f863ae522aca1fc358a1f3ea
ArjunBansil <bansilarjun@gmail.com>
2016-10-22 22:40:04 -0400

    Started some permission work

--- app/src/main/java/com/culinera/culinera/MainActivity.java
+++ app/src/main/java/com/culinera/culinera/MainActivity.java
@@ -1,36 +1,26 @@
 package com.culinera.culinera;
 
 import android.Manifest;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.support.design.widget.Snackbar;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
-import android.text.TextUtils;
-import android.util.Log;
+
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.listener.multi.DialogOnAnyDeniedMultiplePermissionsListener;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
-import com.robotpajamas.blueteeth.BlueteethDevice;
-import com.robotpajamas.blueteeth.BlueteethManager;
+import com.karumi.dexter.listener.single.DialogOnDeniedPermissionListener;
+import com.karumi.dexter.listener.single.PermissionListener;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Dexter.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
         CardView vote = (CardView)findViewById(R.id.signedPic_card);
         CardView view = (CardView)findViewById(R.id.viewData_card);
